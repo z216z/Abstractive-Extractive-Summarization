b22cb6ad2daa7bb7c566de0ee5a5fd7cf3f11834
z216z <39964860+z216z@users.noreply.github.com>
2022-05-24 11:44:40 +0200

    Modified data.py

--- data/data.py
+++ data/data.py
@@ -3,7 +3,7 @@ import json
 import re
 import os
 from os.path import join
-
+import tarfile
 from torch.utils.data import Dataset
 
 
@@ -17,7 +17,7 @@ class CnnDmDataset(Dataset):
         return self._n_data
 
     def __getitem__(self, i: int):
-        #modify the files'path here
+        # modify the files'path here
         with open(join(self._data_path, '{}.json'.format(i))) as f:
             js = json.loads(f.read())
         return js
@@ -26,7 +26,20 @@ class CnnDmDataset(Dataset):
 def _count_data(path):
     """ count number of data in the given path"""
     matcher = re.compile(r'[0-9]+\.json')
-    match = lambda name: bool(matcher.match(name))
+    def match(name): return bool(matcher.match(name))
     names = os.listdir(path)
     n_data = len(list(filter(match, names)))
     return n_data
+# Method for extracting .tar and .tar.gz files
+
+
+def extract_data(path, language):
+    full_path = os.path.join(path, language)
+    if os.path.exists(full_path):
+        for file_path, i in enumerate(os.listdir(full_path)):
+            if file_path.endswith('.tar') or file_path.endswith('.tar.gz'):
+                file = tarfile.open(file_path)
+                file.extractall(full_path)
+                file.close()
+    else:
+        print('The selected dataset is not available')
