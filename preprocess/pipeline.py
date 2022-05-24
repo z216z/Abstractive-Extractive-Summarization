139cff86f8a5dc4fadd4dd06c67b43547a8e54b3
VitoPalmisano <61830892+VitoPalmisano@users.noreply.github.com>
2022-05-24 11:06:12 +0200

    Create pipeline.py

---
+++ preprocess/pipeline.py
@@ -0,0 +1,15 @@
+import os
+import sys
+import json
+from preprocess_methods import tokenizer, generate_bow
+
+"""
+PATH_DATA
+PATH_CORPUS_FILTERED
+PATH_REPORTS_FILTERED
+PATH_SUMMARIES_FILTERED
+PATH_BOW
+"""
+
+def pipeline(PATH_DATA, PATH_CORPUS_FILTERED, PATH_REPORTS_FILTERED, PATH_SUMMARIES_FILTERED, PATH_BOW):
+  
