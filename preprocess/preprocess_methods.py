751e9d267a22f0683d3284b5b11e14e137cfa818
VitoPalmisano <61830892+VitoPalmisano@users.noreply.github.com>
2022-05-26 18:57:08 +0200

    Update regex check

--- preprocess/preprocess_methods.py
+++ preprocess/preprocess_methods.py
@@ -8,8 +8,11 @@ from nltk.tokenize import word_tokenize
 nltk.download('punkt')
 start, end = '<SOS>', '<EOS>'
 tokens = [start, end]
-with open('preprocess/regex.csv') as csvfile:
-    regex = [r for r in csv.reader(csvfile)]
+# Read files for the regex check:
+with open('/content/NLP_Project/preprocess/conversions.csv') as csvfile:
+    conversions = [r for r in csv.reader(csvfile)]
+with open('/content/NLP_Project/preprocess/numerical_related_conversions.csv') as csvfile:
+    numerical_related_conversions = [r for r in csv.reader(csvfile)]
     
 def save_json(bow, file_path):
     json_file = open(file_path, "w")
@@ -25,10 +28,50 @@ def read_json(file_path):
 def filter_sentence(sentence, common_bow):
     return [w for w in sentence.split(" ") if w in common_bow.keys() or w in tokens]
 
+def regex_check(sentence)
+    sentence = re.sub('[^a-zA-Z0-9$£€]', ' ', sentence)
+
+    for row in conversions:
+        abbreviation = row[0]
+        extended_version = row[1]
+
+        sentence = re.sub(f'(^{abbreviation}\s)|(\s{abbreviation}\s)|(\s{abbreviation}$)',
+                          " " + extended_version + " ", sentence)
+
+    for row in numerical_related_conversions:
+        abbreviation = row[0]
+        extended_version = row[1]
+        if abbreviation == "$":
+            abbreviation = "\$"
+
+        sentence = re.sub(f'(^{abbreviation}\s)|(\s{abbreviation}\s)|(\s{abbreviation}$)',
+                          " " + extended_version + " ", sentence)
+
+        match_at_the_beginning = re.search(f'(^{row[0]}\d)', sentence)
+        if match_at_the_beginning is not None:
+            sentence = extended_version + " " + sentence[match_at_the_beginning.end() - 1:]
+
+        match_at_the_end = re.search(f'(\d{row[0]}$)', sentence)
+        if match_at_the_end is not None:
+            sentence = sentence[:match_at_the_end.start() + 1] + " " + extended_version
+
+        matches_in_the_middle = re.finditer(f'(\d{row[0]}\s)|(\s{row[0]}\d)', sentence)
+        next_match_delay = 0
+        for m in matches_in_the_middle:
+            if m is not None:
+                match_start = m.start() + next_match_delay
+                match_end = m.end() + next_match_delay
+                sentence = sentence[:match_start + 1] + " " + extended_version + " " + sentence[match_end - 1:]
+                next_match_delay = len(extended_version) - len(abbreviation)
+
+    sentence = re.sub(f'\s+', " ", sentence)
+    sentence = re.sub(f'^\s|\s$', "", sentence)
+    return sentence
+    
 def tokenize_sentence(sentence, common_bow=None):
     sentence = sentence.lower()
-    for row in regex: 
-        sentence = re.sub(row[0], row[1], sentence)
+    # Use regex to transform abbreviations and acronyms into their corresponding extended form:
+    sentence = regex_check(sentence)
     if common_bow is not None:
         return filter_sentence(sentence, common_bow)
     return word_tokenize(sentence)
