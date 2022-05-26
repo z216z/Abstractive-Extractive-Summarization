fb71d948c61f850ba25b25d8da751dadd7770a8a
Leonardo Maggio <48280683+LeoMaggio@users.noreply.github.com>
2022-05-26 12:33:18 +0200

    Update make_extraction_labels.py

--- preprocess/make_extraction_labels.py
+++ preprocess/make_extraction_labels.py
@@ -1,6 +1,8 @@
 """produce the dataset with (psudo) extraction label"""
 import os
 import json
+import shutil
+from sklearn.model_selection import train_test_split
 from cytoolz import compose
 from NLP_Project import metric
 from metric import compute_rouge_l
@@ -30,7 +32,9 @@ def label(DATASET_PATH):
     path_summaries = os.makedirs(os.path.join(DATASET_PATH, 'preprocess', 'gold_summaries'))
     path_labels = os.makedirs(os.path.join(DATASET_PATH, 'preprocess', 'labels'))
     if not os.path.exists(path_labels):
-        os.makedirs(path_labels)
+        os.makedirs(os.path.join(path_labels, 'all'))
+        os.makedirs(os.path.join(path_labels, 'training'))
+        os.makedirs(os.path.join(path_labels, 'validation'))
         
     for i, file_name in enumerate(os.listdir(path_reports)):
         with open(os.path.join(path_reports, file_name)) as fr:
@@ -43,5 +47,14 @@ def label(DATASET_PATH):
         extracted, scores = get_extract_label(art_sents, abs_sents)
         data['extracted'] = extracted
         data['score'] = scores
-        with open(os.path.join(path_labels, '{}.json'.format(file_name)), 'w') as f:
+        with open(os.path.join(path_labels, 'all', '{}.json'.format(file_name)), 'w') as f:
             json.dump(data, f, indent=4)
+
+def split_data(DATASET_PATH):
+    path_labels = os.makedirs(os.path.join(DATASET_PATH, 'preprocess', 'labels'))
+    file_names = os.listdir(os.path.join(path_labels, 'all'))
+    X_train, X_test, y_train, y_test = train_test_split(file_names, file_names, test_size=0.3, random_state=42)
+    for file_name in X_train:
+        shutil.copyfile(os.path.join(path_labels, 'all', file_name), os.path.join(path_labels, 'training', file_name))
+    for file_name in X_test:
+        shutil.copyfile(os.path.join(path_labels, 'all', file_name), os.path.join(path_labels, 'validation', file_name))
