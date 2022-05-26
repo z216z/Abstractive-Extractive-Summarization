654aefafcf777fb0748b1e3f19b9d5cfaaf7a0a8
Leonardo Maggio <48280683+LeoMaggio@users.noreply.github.com>
2022-05-26 12:45:18 +0200

    Update and rename train_word2vec.py to preprocess/train_word2vec.py

---
+++ preprocess/train_word2vec.py
@@ -0,0 +1,12 @@
+""" pretrain a word2vec on the corpus"""
+import os
+import gensim
+from gensim.models import Word2Vec
+
+def train_word2vec(DATASET_PATH, CORPUS_FILTERED_PATH):
+    corpus=[]
+    with open(CORPUS_FILTERED_PATH) as fr:
+        for line in fr.readlines():
+            corpus.append(line.strip().split(" "))
+    w2v = Word2Vec(corpus, sg=1, min_count=3, window=2, size=300, sample=6e-5, alpha=0.05, negative=20, iter=15)
+    w2v.save(os.path.join(DATASET_PATH, 'preprocess', 'word2vec.model'))      
