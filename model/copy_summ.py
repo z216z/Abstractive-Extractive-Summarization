faf1fd433e612aa0687fbec69da2763c2195c1d6
Leonardo Maggio <48280683+LeoMaggio@users.noreply.github.com>
2022-06-01 12:15:11 +0200

    Update copy_summ.py

--- model/copy_summ.py
+++ model/copy_summ.py
@@ -201,7 +201,7 @@ class CopyLSTMDecoder(AttentionalLSTMDecoder):
             ).scatter_add(
                 dim=1,
                 index=extend_src.expand_as(score),
-                source=score * copy_prob
+                src=score * copy_prob
         ) + 1e-8)  # numerical stability for log
         return lp, (states, dec_out), score
 
