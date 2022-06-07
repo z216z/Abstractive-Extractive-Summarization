b27427c7bcf90fcee84d3fa2311fa63a45d02daa
Leonardo Maggio <48280683+LeoMaggio@users.noreply.github.com>
2022-06-07 11:37:07 +0200

    Update decode_full_model.py

--- decode_full_model.py
+++ decode_full_model.py
@@ -9,48 +9,54 @@ from collections import Counter, defaultdict
 from itertools import product
 from functools import reduce
 import operator as op
-
+from toolz.sandbox import unzip
 from cytoolz import identity, concat, curry
 
 import torch
 from torch.utils.data import DataLoader
 from torch import multiprocessing as mp
-
+from statistics import mean
 from data.batcher import tokenize
+from metric import compute_rouge_n, compute_rouge_l_summ
+from decoding import Abstractor, RLExtractor, DecodeDataset
 
-from decoding import Abstractor, RLExtractor, DecodeDataset, BeamAbstractor
-from decoding import make_html_safe
+DATASET_PATH = None
+DATA_DIR = None
 
+def compute_rouges(dec_outs, gold):
+    rouge_1 = compute_rouge_n(list(concat(dec_outs)),list(concat(gold)), n=1))
+    rouge_2 = compute_rouge_n(list(concat(dec_outs)),list(concat(gold)), n=2))
+    rouge_L = compute_rouge_l_summ(dec_outs, gold)
+    return rouge_1, rouge_2, rouge_L
 
-def decode(save_path, model_dir, split, batch_size,
-           beam_size, diverse, max_len, cuda):
+def decode(model_dir, batch_size, max_len, cuda):
+    split = 'test' 
+    model_dir = join(DATASET_PATH, model_dir)
+    save_path = join(model_dir, 'eval')
     start = time()
     # setup model
-    with open(join(model_dir, 'meta.json')) as f:
+    with open(join(model_dir, 'rl', 'meta.json')) as f:
         meta = json.loads(f.read())
     if meta['net_args']['abstractor'] is None:
         # NOTE: if no abstractor is provided then
         #       the whole model would be extractive summarization
-        assert beam_size == 1
         abstractor = identity
     else:
-        if beam_size == 1:
-            abstractor = Abstractor(join(model_dir, 'abstractor'),
-                                    max_len, cuda)
-        else:
-            abstractor = BeamAbstractor(join(model_dir, 'abstractor'),
-                                        max_len, cuda)
-    extractor = RLExtractor(model_dir, cuda=cuda)
+        abstractor = Abstractor(join(model_dir, 'abs'),
+                                max_len, cuda)
+    extractor = RLExtractor(join(model_dir, 'ext'), cuda=cuda)
 
     # setup loader
-    def coll(batch):
-        articles = list(filter(bool, batch))
-        return articles
-    dataset = DecodeDataset(split)
+    def coll(data):
+        source_lists, target_lists = unzip(data)
+        sources = list(filter(bool, map(tokenize(None), source_lists)))
+        targets = list(filter(bool, map(tokenize(None), target_lists)))
+        return sources, targets
+    dataset = DecodeDataset(DATA_DIR, split)
 
     n_data = len(dataset)
     loader = DataLoader(
-        dataset, batch_size=batch_size, shuffle=False, num_workers=4,
+        dataset, batch_size=batch_size, shuffle=False, num_workers=0,
         collate_fn=coll
     )
 
@@ -60,20 +66,17 @@ def decode(save_path, model_dir, split, batch_size,
     dec_log['abstractor'] = meta['net_args']['abstractor']
     dec_log['extractor'] = meta['net_args']['extractor']
     dec_log['rl'] = True
-    dec_log['split'] = split
-    dec_log['beam'] = beam_size
-    dec_log['diverse'] = diverse
     with open(join(save_path, 'log.json'), 'w') as f:
         json.dump(dec_log, f, indent=4)
 
     # Decoding
     i = 0
+    rouges = {'rouge_1': [], 'rouge_2': [], 'rouge_L': []}
     with torch.no_grad():
-        for i_debug, raw_article_batch in enumerate(loader):
-            tokenized_article_batch = map(tokenize(None), raw_article_batch)
+        for article_batch, gold_batch in loader:
             ext_arts = []
             ext_inds = []
-            for raw_art_sents in tokenized_article_batch:
+            for raw_art_sents in article_batch:
                 ext = extractor(raw_art_sents)[:-1]  # exclude EOE
                 if not ext:
                     # use top-5 if nothing is extracted
@@ -83,78 +86,37 @@ def decode(save_path, model_dir, split, batch_size,
                     ext = [i.item() for i in ext]
                 ext_inds += [(len(ext_arts), len(ext))]
                 ext_arts += [raw_art_sents[i] for i in ext]
-            if beam_size > 1:
-                all_beams = abstractor(ext_arts, beam_size, diverse)
-                dec_outs = rerank_mp(all_beams, ext_inds)
-            else:
-                dec_outs = abstractor(ext_arts)
-            assert i == batch_size*i_debug
-            for j, n in ext_inds:
+            
+            dec_outs = abstractor(ext_arts)
+            for (j, n), gold in zip(ext_inds, gold_batch):
                 decoded_sents = [' '.join(dec) for dec in dec_outs[j:j+n]]
-                with open(join(save_path, 'output/{}.dec'.format(i)),
-                          'w') as f:
-                    f.write(make_html_safe('\n'.join(decoded_sents)))
+                with open(join(save_path, 'output/{}.txt'.format(i)), 'w') as f:
+                    f.write('\n'.join(decoded_sents))
                 i += 1
-                print('{}/{} ({:.2f}%) decoded in {} seconds\r'.format(
-                    i, n_data, i/n_data*100,
-                    timedelta(seconds=int(time()-start))
-                ), end='')
-    print()
+                print('{}/{} ({:.2f}%) decoded in {} seconds\r'.format(i, n_data, i/n_data*100,timedelta(seconds=int(time()-start))), end='')
+                
+                for index, sent in enumerate(gold):
+                    gold[index] = sent[1:-1]
+                doc_rouges = compute_rouges(dec_outs, gold)
+                for value, key in zip(doc_rouges, rouges.keys()):
+                    rouges[key].append(value)
+    
+    for key in rouges.keys():
+        print(f'Average ROUGE-{key.split('_')[-1]}: {mean(rouges[key])}')
 
-_PRUNE = defaultdict(
-    lambda: 2,
-    {1:5, 2:5, 3:5, 4:5, 5:5, 6:4, 7:3, 8:3}
-)
-
-def rerank(all_beams, ext_inds):
-    beam_lists = (all_beams[i: i+n] for i, n in ext_inds if n > 0)
-    return list(concat(map(rerank_one, beam_lists)))
-
-def rerank_mp(all_beams, ext_inds):
-    beam_lists = [all_beams[i: i+n] for i, n in ext_inds if n > 0]
-    with mp.Pool(8) as pool:
-        reranked = pool.map(rerank_one, beam_lists)
-    return list(concat(reranked))
-
-def rerank_one(beams):
-    @curry
-    def process_beam(beam, n):
-        for b in beam[:n]:
-            b.gram_cnt = Counter(_make_n_gram(b.sequence))
-        return beam[:n]
-    beams = map(process_beam(n=_PRUNE[len(beams)]), beams)
-    best_hyps = max(product(*beams), key=_compute_score)
-    dec_outs = [h.sequence for h in best_hyps]
-    return dec_outs
-
-def _make_n_gram(sequence, n=2):
-    return (tuple(sequence[i:i+n]) for i in range(len(sequence)-(n-1)))
-
-def _compute_score(hyps):
-    all_cnt = reduce(op.iadd, (h.gram_cnt for h in hyps), Counter())
-    repeat = sum(c-1 for g, c in all_cnt.items() if c > 1)
-    lp = sum(h.logprob for h in hyps) / sum(len(h.sequence) for h in hyps)
-    return (-repeat, lp)
-
-
+    
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='run decoding of the full model (RL)')
-    parser.add_argument('--path', required=True, help='path to store/eval')
-    parser.add_argument('--model_dir', help='root of the full model')
-
-    # dataset split
-    data = parser.add_mutually_exclusive_group(required=True)
-    data.add_argument('--val', action='store_true', help='use validation set')
-    data.add_argument('--test', action='store_true', help='use test set')
+    parser.add_argument('--data', type=str, default='FNS2022', choices={'FNS2022', 'CNN'}, help='Select the dataset.')
+    parser.add_argument('--language', type=str, default='English', choices={'English', 'Greek', 'Spanish'}, help='Select the language if you use FNS2022.')
+    
+    parser.add_argument('--model_dir', action='store', default='model', 
+                        help='root of the full model')
 
     # decode options
-    parser.add_argument('--batch', type=int, action='store', default=32,
+    parser.add_argument('--batch', type=int, action='store', default=2,
                         help='batch size of faster decoding')
-    parser.add_argument('--beam', type=int, action='store', default=1,
-                        help='beam size for beam-search (reranking included)')
-    parser.add_argument('--div', type=float, action='store', default=1.0,
-                        help='diverse ratio for the diverse beam-search')
     parser.add_argument('--max_dec_word', type=int, action='store', default=30,
                         help='maximun words to be decoded for the abstractor')
 
@@ -163,7 +125,13 @@ if __name__ == '__main__':
     args = parser.parse_args()
     args.cuda = torch.cuda.is_available() and not args.no_cuda
 
-    data_split = 'test' if args.test else 'val'
-    decode(args.path, args.model_dir,
-           data_split, args.batch, args.beam, args.div,
+    DATASET_PATH = '/content/NLP_Project/Dataset'
+    if args.data == 'FNS2022':
+        LANGUAGE = args.language
+    else:
+        LANGUAGE = 'English'
+    DATASET_PATH = os.path.join(DATASET_PATH, args.data, LANGUAGE)
+    DATA_DIR = os.path.join(DATASET_PATH, 'preprocess', 'labels')
+    
+    decode(args.model_dir, args.batch,
            args.max_dec_word, args.cuda)
