00403ecf2694bc17f57ca3a7238c7e62d3e7b711
VitoPalmisano <61830892+VitoPalmisano@users.noreply.github.com>
2022-06-23 12:54:19 +0200

    Update train_word2vec.py

--- preprocess/train_word2vec.py
+++ preprocess/train_word2vec.py
@@ -8,5 +8,5 @@ def train_word2vec(DATASET_PATH, CORPUS_FILTERED_PATH, emb_dim):
     with open(CORPUS_FILTERED_PATH) as fr:
         for line in fr.readlines():
             corpus.append(line.strip().split(" "))
-    w2v = Word2Vec(corpus, sg=1, min_count=3, window=2, size=emb_dim, sample=6e-5, alpha=0.05, negative=20, iter=15)
+    w2v = Word2Vec(corpus, sg=1, min_count=3, window=2, size=emb_dim, sample=6e-5, alpha=0.05, negative=20, epochs=15)
     w2v.save(os.path.join(DATASET_PATH, 'preprocess', 'word2vec.model'))      
