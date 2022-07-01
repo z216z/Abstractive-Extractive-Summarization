db3bb5f1fbfc228b3d0e42596a20d245abc42a86
Leonardo Maggio <48280683+LeoMaggio@users.noreply.github.com>
2022-07-01 13:40:59 +0200

    Update pipeline.py

--- preprocess/pipeline.py
+++ preprocess/pipeline.py
@@ -49,11 +49,12 @@ def pipeline(DATASET_PATH, LANGUAGE, STAGE):
             common_bow = read_json(os.path.join(DATASET_PATH, 'preprocess', 'common_bow.json'))
         except ValueError:
             return False
-        for split in ['training', 'validation']:
-            for folder in ['annual_reports', 'gold_summaries']:
-                for file_name in tqdm(os.listdir(os.path.join(DATASET_PATH, split, folder))):
-                    tokenizer(os.path.join(DATASET_PATH, split, folder, file_name), os.path.join(DATASET_PATH, 'preprocess', split, folder, file_name), LANGUAGE, common_bow)
-                print(f'{split} {folder} processed!')
+        for split in ['training', 'validation', 'test']:
+            if os.path.exists(os.path.join(DATASET_PATH, split):
+                for folder in ['annual_reports', 'gold_summaries']:
+                    for file_name in tqdm(os.listdir(os.path.join(DATASET_PATH, split, folder))):
+                        tokenizer(os.path.join(DATASET_PATH, split, folder, file_name), os.path.join(DATASET_PATH, 'preprocess', split, folder, file_name), LANGUAGE, common_bow)
+                    print(f'{split} {folder} processed!')
         STAGE = 4
     
     if STAGE == 4 and os.path.exists(CORPUS_FILTERED_PATH):
@@ -66,12 +67,16 @@ def pipeline(DATASET_PATH, LANGUAGE, STAGE):
         len(os.listdir(os.path.join(DATASET_PATH, 'preprocess', 'training', 'gold_summaries'))) > 0 and \
         len(os.listdir(os.path.join(DATASET_PATH, 'preprocess', 'validation', 'annual_reports'))) > 0 and \
         len(os.listdir(os.path.join(DATASET_PATH, 'preprocess', 'validation', 'gold_summaries'))) > 0:
-        for _, split in enumerate(['training', 'validation']):
-            label(DATASET_PATH, split, args.jit)
-            split = 'train' if split == 'training' else 'test'
-            print(f'Labels generated for the {split} set!')
-        split_data(os.path.join(DATASET_PATH, 'preprocess', 'labels'))
-        print(f'Labels generated for the validation set!')
+        task = args.task if args.data == 'CNN' else None
+        for split in ['training', 'validation', 'test']:
+            if os.path.exists(os.path.join(DATASET_PATH, split):
+                label(DATASET_PATH, split, args.jit, task)
+                if args.data == 'FNS2022':
+                    split = 'train' if split == 'training' else 'test'
+                print(f'Labels generated for the {split} set!')
+        if args.data == 'FNS2022':
+            split_data(os.path.join(DATASET_PATH, 'preprocess', 'labels'))
+            print(f'Labels generated for the validation set!')
       
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -80,6 +85,7 @@ if __name__ == '__main__':
     parser.add_argument('--data', type=str, default='FNS2022', choices={'FNS2022', 'CNN'}, help='Select the dataset.')
     parser.add_argument('--language', type=str, default='English', choices={'English', 'Greek', 'Spanish'}, help='Select the language if you use FNS2022.')
     parser.add_argument('--stage', type=int, default=0, choices={0, 1, 2, 3, 4, 5}, help=stage_help)
+    parser.add_argument('--task', type=str, default='Headline Generation', choices={'Headline Generation', 'Summarization'}, help='Select the task to carry over the CNN dataset.')
     parser.add_argument('--emb_dim', type=int, default=300, action='store', help='The dimension of word embedding.')
     parser.add_argument('--max_len', type=int, default=1000, action='store', help='Limit the number of sentences in the articles for training purposes.')
     parser.add_argument('--jit', action="store_true", help='Optimize runtime performance using parallelization.')
